bd4e5d2152642db0c3814533a590a8e49c37208e
Eomjangheon <mpmpm0610@gmail.com>
2022-11-25 14:08:59 +0900

    변수 관리용 서버 추가

---
+++ CG_TeamProj/src/server.cpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "gameWorld.h"
+#include "light.h"
+#include "camera.h"
+#include "wall.h"
+#include "player.h"
+#include "gameManager.h"
+#include "object.h"
+#include "stage.h"
+#include <vector>
+
+GameWorld gameWorld;
+Light light;
+Camera camera;
+Player player;
+Wall wall;
+Wall* wallPtr;
+GameManager gameManager;
+Object* playerPtr = &player;
+int nowStage = 0;
+vector<Stage*> stages;
