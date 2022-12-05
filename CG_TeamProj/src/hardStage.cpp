f50912d2798d4f35c7005a158b1297312e1dfc35
Eomjangheon <mpmpm0610@gmail.com>
2022-12-05 13:01:09 +0900

    feat: stage별 카메라 회전

--- CG_TeamProj/src/hardStage.cpp
+++ CG_TeamProj/src/hardStage.cpp
@@ -5,33 +5,50 @@
 #include "gameWorld.h"
 #include "camera.h"
 #include "wall.h"
+#include "particle.h"
 extern Player player;
 extern Light light;
 extern GameWorld gameWorld;
 extern Camera camera;
 extern GLuint shaderID;
 extern Object* playerPtr;
+extern Wall wall;
 
 void HardStage::init()
 {
     cout << "hard Stage" << endl;
     player.initBuffer();
-    player.colorInit();
+    player.initTexture();
     gameWorld.add_object(playerPtr);
 
-    //for (int i = 0; i < 1; ++i)
-    //{
-    //    cout << i << endl;
-    //    Wall* tempwall = new Wall();
-    //    tempwall->initBuffer();
-    //    //tempwall->colorInit();
-    //    gameWorld.add_object(tempwall);
-    //}
+    /*for (int i = 0; i < 100; ++i)
+    {
+        cout << i << endl;
+        Wall *tempwall = new Wall();
+        tempwall->initBuffer();
+        tempwall->initTexture();
+        gameWorld.add_object(tempwall);
+    }*/
+    makePattern(1);
+    for (int i = 0; i < 20; ++i)
+    {
+        cout << i << endl;
+        Particle* tempP = new Particle(true);
+        tempP->initBuffer();
+        gameWorld.add_object(tempP);
+    }
 }
 void HardStage::update()
 {
     gameWorld.update_all();
     light.update(); // ����
+    timer++;
+    // Camera rolling test
+    if(timer%200<100)
+        camera.rolling((timer%100*0.01+0.5), -1); // angle, direction
+    else
+        camera.rolling((timer % 100 * 0.01+0.5), 1);
+
     glutPostRedisplay();
 }
 void HardStage::handleEvent(unsigned char key, bool isDown)
@@ -40,7 +57,10 @@ void HardStage::handleEvent(unsigned char key, bool isDown)
     {
         switch (key)
         {
-
+        case'c':
+            makePattern(1);
+            //player.setProtectedMode(true);
+            break;
         case 'a':
             player.setMoveLeft(true);
             break;
