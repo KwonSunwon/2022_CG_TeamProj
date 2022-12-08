a4733c28991807877efe1b9ecb43e8305bed51b7
KwonSunwon <kso00011147@gmail.com>
2022-12-08 11:41:28 +0900

    feat: 메인 메뉴 클래스 제작

--- CG_TeamProj/src/menu.h
+++ CG_TeamProj/src/menu.h
@@ -5,7 +5,20 @@
 
 class Menu : public Object
 {
-    
+private:
+    vector<glm::vec3> vertices;
+    vector<glm::vec3> normals;
+    vector<glm::vec2> uvs;
+    GLuint texture;
+
+public:
+    Menu();
+    void render(GLuint shaderProgramID) override;
+    void initBuffer() override;
+    void initTexture() override;
+    void update() override;
+
+    void clicked(int x, int y);
 };
 
 #endif
